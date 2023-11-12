664fae6a9e3ddc97ae8e0ca0f2ea443742672cd5
Sav-MI <saviour08100304152@gmail.com>
2023-11-12 18:22:24 +0000

    updated

--- getline.c
+++ getline.c
@@ -1,12 +1,44 @@
 #include "shell_header.h"
 
+void exe(char **s)
+{
+	pid_t pchild;
+	int status;
+	int oo = 2;
+	fflush(stdout);
+	fflush(stdin);
+	
+	switch ((pchild = fork()))
+	{
+	case -1:
+		perror("fork");
+		break;
+	case 0:
+		errno = 0;
+		execve(s[0], s, environ);
+		perror("execve");
+		exit(EXIT_SUCCESS);
+	default:
+		do {
+			waitpid(pchild, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return;
+}
 int main(){
-	char *s = "hello";
-	int c;
+	char *s[3] = {"/bin/watch", "p", NULL};
 
-	while (!(c = delimCharcmp("/", s++)))
-		putchar(*s);
-	if (c == 1)
-		puts("found");
-	return 0;
+	pid_t pchild;
+	int status;
+	int oo = 2;
+	fflush(stdout);
+	fflush(stdin);
+
+	while (oo--)
+	{
+		exe(s);
+		s[0] = "/bin/ls";
+		s[1] = "-l";
+		exe(s);
+	}
 }
