e67fd33224041313a2dfe0fa35bcf09264e53a71
saviour623 <saviourme33@gmail.com>
2023-11-13 18:46:50 +0100

    updated

--- errmsg.c
+++ errmsg.c
@@ -27,7 +27,7 @@ const char *GLOBAL_SHELL_RECG_ERROR_MSG[27] = {
 void errMsg(int errnum, struct shell_info *procinfo)
 {
 	char s[256];
-	register size_t len, procnum, oo = 0;
+	register size_t len, procnum, oo = 0, pp = 0;
 
 	len = str_cpy(s, procinfo->shell_name, 0);
 	parse_colmn_space_tstr(s, len);
@@ -38,22 +38,24 @@ void errMsg(int errnum, struct shell_info *procinfo)
 		procnum /= 10;
 
 	procnum = procinfo->cmd_cnt;
+	len += (oo - 1);
 	while (procnum)
 	{
-		puts("in");
-		*((s + len) + oo) = (procnum % 10) - 48;
+		*(s + len) = (procnum % 10) + 48;
 		procnum /= 10;
-		oo--;
+		len--;
 	}
-	len += oo;
+	len += (oo + 1);
+	parse_colmn_space_tstr(s, len);
+
 	len += str_cpy((s + len), procinfo->cmd, 0);
 	parse_colmn_space_tstr(s, len);
 
 	s[len] = 0;
 	puts(s);
 
-	//_nputs(s, 0);
-//	_nputs(GLOBAL_SHELL_RECG_ERROR_MSG[errnum], 1); */
+	_nputs(s, 0);
+	_nputs(GLOBAL_SHELL_RECG_ERROR_MSG[errnum], 1);
 }
 
 size_t _nputs(const char *str, int newline)
